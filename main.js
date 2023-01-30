0f898527652848462ae5046dc9d3b46ca5dab21c
rhoyler7 <roberthoyler@gmail.com>
2023-01-30 15:55:39 -0500

    Title / Header

---
+++ main.js
@@ -0,0 +1,20 @@
+// JS File for Class Demo
+
+// print
+console.log("hello_world");
+
+// variables
+
+// constants
+const myCourse = "DS4200";
+console.log(myCourse);
+
+let season = "Winter";
+consol.log(season);
+
+season = "Spring";
+consol.log(season);
+
+// don't use var
+
+let hwDue = true;
